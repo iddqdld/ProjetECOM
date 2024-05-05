c67e664fb79480621532e94a8cf1d23a9681b446
iddqdld <152985414+iddqdld@users.noreply.github.com>
2024-05-05 21:39:06 +0200

    login update

--- login.php
+++ login.php
@@ -5,12 +5,13 @@ include('db.php');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
+    $passwordSha256 = hash('sha256', $password);
 
     // Vérifiez les informations d'identification dans la base de données
     $query = "SELECT * FROM users WHERE username = :username AND password = :password";
     $stmt = $pdo->prepare($query);
     $stmt->bindParam(':username', $username, PDO::PARAM_STR);
-    $stmt->bindParam(':password', $password, PDO::PARAM_STR);
+    $stmt->bindParam(':password', $passwordSha256, PDO::PARAM_STR);
     $stmt->execute();
 
     if ($user = $stmt->fetch(PDO::FETCH_ASSOC)) {
@@ -37,6 +38,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <p style="color: red;"><?php echo $error_message; ?></p>
     <?php endif; ?>
 
+    <div>
+        Pas encore inscrit ? <a href="#">S'inscrire</a>
+    </div>
+
     <form method="post" action="">
         <label for="username">Username:</label>
         <input type="text" name="username" required><br>
@@ -44,7 +49,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <input type="password" name="password" required><br>
         <button type="submit">Login</button>
     </form>
-    <a href="save.php">Enregistrer</a>
+
+    <div>
+        <a href="save.php">Changer son mot de passe</a>
+    </div>
 
 
 
